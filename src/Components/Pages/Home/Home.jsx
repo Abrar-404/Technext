39ece9a3d741809744a3c63526bab22c461009fd
Sahil Abrar <zeroxkhan2@gmail.com>
2024-02-09 20:30:06 +0600

    added head text

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,11 +1,17 @@
 import Banner from '../../Banner/Banner';
 import UserCardFunc from '../Users/UserCardFunc';
+import '../../Styles/text.css'
 
 const Home = () => {
   return (
     <div>
       <Banner></Banner>
-      <UserCardFunc></UserCardFunc>
+      <div className="mt-32">
+        <h1 className="flex justify-center text-3xl font-fancy text-flicker-in-glow font-bold">
+          Existing Users
+        </h1>
+        <UserCardFunc></UserCardFunc>
+      </div>
     </div>
   );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ export default {
     extend: {
       fontFamily: {
         sans: ['Josefin Sans', 'sans-serif'],
+        fancy: ['Yatra One', 'system-ui'],
       },
     },
   },
